700613d889d2aa78c4ad8b13f5eedfcaa343d44c
Oscar Durán <19002632@alumnos.utleon.edu.mx>
2023-09-18 19:32:52 -0600

    18/09/23 OADO

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'AngularEjem';
+
+  apellido='Duran';
+
+  duplicarNumero(valor:number):number{
+    return valor*2;
+  }
+  pelicula={
+    titulo:'Spiderman',
+    fechaLanzamiento:new Date(),
+    precio: 75.56
+  }
+}
