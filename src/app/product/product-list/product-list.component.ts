7a47b417cb4d3a38e7fb8712026a31ae5bdbc31a
Oscar Durán <19002632@alumnos.utleon.edu.mx>
2023-09-22 18:43:47 -0600

    22.09.23 OADO

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Iproductos } from '../iproductos';
 
 @Component({
   selector: 'app-product-list',
@@ -7,7 +8,17 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
   //directivas *nglif
-productos:any[]=[
+
+  imageWidth:number=50;
+  imagenMargin:number=2;
+  muestraImg:boolean=true;
+  listFilter:string='';
+
+  showImage():void{
+    this.muestraImg=!this.muestraImg;
+  }
+
+productos:Iproductos[]=[
   {
     "productoId":1,
     "Modelo":"Sentra",
@@ -16,27 +27,27 @@ productos:any[]=[
     "Precio":120000,
     "Marca":"NISSAN",
     "Color":"Morado",
-    "imagenUrl":"datos pendientes"
+    "imagenUrl":"https://img.automexico.com/2021/03/01/fc414a41-06cf.jpg"
   },
   {
-    "productoId":1,
+    "productoId":5,
     "Modelo":"A4",
     "Descripcion":"4 puertas familiar",
     'year':"febrero 3 2020",
     "Precio":150000,
     "Marca":"AUDI",
-    "Color":"Blanco",
-    "imagenUrl":"datos pendientes"
+    "Color":"Gris",
+    "imagenUrl":"https://img.remediosdigitales.com/d46bf6/audi-a4-2020-precio-mexico_/1366_2000.jpg"
   },
   {
     "productoId":3,
     "Modelo":"RIO",
     "Descripcion":"2 puertas familiar",
-    'year':"mARZO 5 2022",
+    'year':"Marzo 5 2022",
     "Precio":150000,
     "Marca":"KIA",
-    "Color":"azul",
-    "imagenUrl":"datos pendientes"
+    "Color":"Blanco",
+    "imagenUrl":"https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/2018_Kia_Rio_EX_-_Side.jpg/1200px-2018_Kia_Rio_EX_-_Side.jpg"
   }
 ]
 }
